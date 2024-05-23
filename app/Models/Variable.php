280d6eb762ba2f2b675cfc99987b0aac7fb23744
carloeusebi <carloeusebi@gmail.com>
2024-05-23 23:11:41 +0200

    Created Variable model.

---
+++ app/Models/Variable.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Variable extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'questionnaire_id',
+        'name',
+    ];
+
+    public function questionnaire(): BelongsTo
+    {
+        return $this->belongsTo(Questionnaire::class);
+    }
+
+    public function questions(): HasMany
+    {
+        return $this->hasMany(Question::class);
+    }
+}
---
+++ database/migrations/2024_05_23_210637_create_variables_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Question;
+use App\Models\Questionnaire;
+use App\Models\Variable;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('variables', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Questionnaire::class)->constrained()->cascadeOnDelete();
+            $table->string('name');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+
+        Schema::create('question_variable', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Question::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Variable::class)->constrained()->cascadeOnDelete();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('variables');
+    }
+};
