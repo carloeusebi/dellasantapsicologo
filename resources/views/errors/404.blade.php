03991fa03e4c59c1dfa50d09503515d9be5fcda1
carloeusebi <carloeusebi@gmail.com>
2024-05-23 18:40:56 +0200

    Added 404 page for auth

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,6 +1,19 @@
-<x-layouts.main>
-  <div class="text-align-center mt-20">
-    <h1 class="p-20">404 - PAGINA NON TROVATA</h1>
-    <a class='btn' href="{{ route('home') }}">torna alla homepage</a>
-  </div>
-</x-layouts.main>
+@guest()
+  <x-layouts.main>
+    <div class="text-align-center mt-20">
+      <h1 class="p-20">404 - PAGINA NON TROVATA</h1>
+      <a class='btn' href="{{ route('home') }}">torna alla homepage</a>
+    </div>
+  </x-layouts.main>
+@endguest
+@auth()
+  <x-layouts.app>
+    <h1 class="text-3xl font-bold mb-2">
+      <x-heroicon-o-exclamation-triangle class="w-10 h-10 inline"/>
+      Pagina non trovata
+    </h1>
+    <div>
+      <span>Sembra che questa pagina non sia più disponibile.</span>
+    </div>
+  </x-layouts.app>
+@endauth
