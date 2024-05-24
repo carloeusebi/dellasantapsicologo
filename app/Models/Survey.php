28d637e0f2e65d4380b9df03891e347c8232c198
carloeusebi <carloeusebi@gmail.com>
2024-05-24 10:45:00 +0200

    Added soft deletes to surveys table

--- app/Models/Survey.php
+++ app/Models/Survey.php
@@ -5,9 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Survey extends Model
 {
+    use SoftDeletes;
+
     public function patient(): BelongsTo
     {
         return $this->belongsTo(Patient::class);
