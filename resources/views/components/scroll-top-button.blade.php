06b76d2e627fddc7b658e4b3c9a8107e5a7222f3
carloeusebi <carloeusebi@gmail.com>
2024-06-01 18:12:21 +0200

    Improved scroll to top button style

--- resources/views/components/scroll-top-button.blade.php
+++ resources/views/components/scroll-top-button.blade.php
@@ -1,13 +1,13 @@
 <div
     x-data="{ show: false }"
     x-on:scroll.window="show = window.scrollY >= 800"
-    class="fixed bottom-8 right-8"
+    class="fixed bottom-4 right-4"
 >
   <button
       x-show="show"
       x-transition
       x-on:click="window.scrollTo({ top: 0, behavior: 'smooth' })"
-      class="btn btn-secondary btn-sm shadow-2xl z-50 btn-circle"
+      class="btn btn-secondary shadow-2xl z-50 btn-circle"
   >
     <x-icon name="o-arrow-up"/>
   </button>
