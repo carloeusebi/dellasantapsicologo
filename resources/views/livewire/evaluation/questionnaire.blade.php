c2031e62ce2210e309dc1bba667571de4fdcbf69
carloeusebi <carloeusebi@gmail.com>
2024-06-04 16:22:07 +0200

    Fixed patient could change answer before animation

--- resources/views/livewire/evaluation/questionnaire.blade.php
+++ resources/views/livewire/evaluation/questionnaire.blade.php
@@ -38,27 +38,44 @@
             class="progress-primary" :value="$question->order - 1" :max="$questionnaireSurvey->questions_count"
         />
       </div>
-      @foreach($question->choices->isNotEmpty() ? $question->choices :  $questionnaireSurvey->questionnaire->choices as $choice)
-        <div
-            wire:key="{{  $question->id }}-{{ $choice->id }}"
-            x-data=" { selected: false }"
-            :class=" { active: selected }"
-            class="choice relative flex items-center min-h-[40px] w-full mb-2 py-1 pe-3 rounded bg-brand/20
-            text-brand-secondary shadow cursor-pointer border border-brand-secondary hover:scale-105 transition-all"
-            data-choice-id="{{ $choice->id }}"
-            wire:keyup.{{ $loop->index + 1 }}.window="answerQuestion({{ $question->id }}, {{ $choice->id }})"
-            wire:click="answerQuestion({{ $question->id }}, {{ $choice->id }})"
-            x-on:keyup.{{ $loop->index + 1 }}.window="selected = true"
-            x-on:click="selected = true"
-        >
+      <div
+          :key="{{ $question->id}}"
+          x-data="{
+          answered: false,
+          answerQuestion(questionId, choiceId) {
+            if (this.answered) return;
+            this.answered = true;
+            $wire.answerQuestion(questionId, choiceId);
+          }
+        }"
+      >
+        @foreach($question->choices->isNotEmpty() ? $question->choices :  $questionnaireSurvey->questionnaire->choices as $choice)
           <div
-              class="w-6 h-6 my-1 mx-2 flex items-center justify-center rounded-sm border border-brand-secondary bg-white/80 shrink-0"
-          >{{ $loop->index + 1 }}</div>
-          <div class="text-start">{{ $choice->text }}</div>
-          <div class="grow"></div>
-          <x-icon name="o-check" class="check-icon hidden"/>
-        </div>
-      @endforeach
+              wire:key="{{  $question->id }}-{{ $choice->id }}"
+              x-data=" {
+                selected: false,
+                selectChoice() {
+                if (answered) return;
+                  this.selected = true;
+                  answerQuestion({{ $question->id }}, {{ $choice->id }});
+                }
+              }"
+              :class=" { active: selected }"
+              class="choice relative flex items-center min-h-[40px] w-full mb-2 py-1 pe-3 rounded bg-brand/20
+            text-brand-secondary shadow cursor-pointer border border-brand-secondary hover:scale-105 transition-all"
+              data-choice-id="{{ $choice->id }}"
+              x-on:keyup.{{ $loop->index + 1 }}.window="selectChoice()"
+              x-on:click="selectChoice()"
+          >
+            <div
+                class="w-6 h-6 my-1 mx-2 flex items-center justify-center rounded-sm border border-brand-secondary bg-white/80 shrink-0"
+            >{{ $loop->index + 1 }}</div>
+            <div class="text-start">{{ $choice->text }}</div>
+            <div class="grow"></div>
+            <x-icon name="o-check" class="check-icon hidden"/>
+          </div>
+        @endforeach
+      </div>
     </div>
   </template>
 </div>
