f4c98f79ea7a7933c03b797ffbec086547c4baa7
carloeusebi <carloeusebi@gmail.com>
2024-06-06 18:10:03 +0200

    Added browsershot lambda

--- app/Http/Controllers/DownloadSurveyController.php
+++ app/Http/Controllers/DownloadSurveyController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Survey;
 use Illuminate\Support\Str;
 use Spatie\Browsershot\Browsershot;
+use Wnx\SidecarBrowsershot\BrowsershotLambda;
 use function Spatie\LaravelPdf\Support\pdf;
 
 class DownloadSurveyController extends Controller
@@ -18,16 +19,15 @@ class DownloadSurveyController extends Controller
             'questionnaireSurveys.questionnaire.questions.choices.questionable',
         );
 
-//        return view('pdf.survey', compact('survey'));
-
         return pdf()
             ->view('pdf.survey', compact('survey'))
             ->withBrowsershot(function (Browsershot $browsershot) {
-                $browsershot
-                    ->setNpmBinary(config('services.browsershot.npm_path'))
-                    ->setNodeBinary(config('services.browsershot.node_path'));
+                $browsershot = new BrowsershotLambda();
 
             })
             ->name(Str::kebab("$survey->title di {$survey->patient->full_name}.pdf"));
+
+//        BrowsershotLambda::html(view('pdf.survey', compact('survey')))
+//            ->save(Str::kebab("$survey->title di {$survey->patient->full_name}.pdf"));
     }
 }
