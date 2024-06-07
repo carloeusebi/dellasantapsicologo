65f89cdd356b8a6d44446ceb8b83915b6e2c2b01
carloeusebi <carloeusebi@gmail.com>
2024-06-07 08:47:59 +0200

    Updated vite config

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,6 @@ export default defineConfig({
   plugins: [
     laravel({
       input: [
-        'resources/css/style.scss',
-        'resources/css/my-library.css',
         'resources/css/app.css',
         'resources/css/evaluation.css',
         'resources/js/app.js',
