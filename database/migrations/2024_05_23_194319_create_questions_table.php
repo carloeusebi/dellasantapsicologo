25ebda3e4294638ad2c22c23738e0f91cc086bea
carloeusebi <carloeusebi@gmail.com>
2024-06-10 23:38:38 +0200

    Changed questions migration

--- database/migrations/2024_05_23_194319_create_questions_table.php
+++ database/migrations/2024_05_23_194319_create_questions_table.php
@@ -13,7 +13,7 @@ return new class extends Migration {
             Schema::create('questions', function (Blueprint $table) {
                 $table->id();
                 $table->foreignIdFor(Questionnaire::class)->constrained()->cascadeOnDelete();
-                $table->text('text');
+                $table->text('text')->nullable();
                 $table->boolean('reversed')->default(false);
                 $table->json('custom_choices')->nullable();
                 $table->tinyInteger('order')->default(0);
