4c01000ac1a21af7483a6277afbc8143f74cc853
carloeusebi <carloeusebi@gmail.com>
2024-06-10 23:47:35 +0200

    Cleaned questions table

---
+++ database/migrations/2024_06_10_214435_celanup_questions_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->text('text')->nullable(false)->change();
+            $table->dropColumn('custom_choices');
+            $table->dropColumn('old_id');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->text('text')->nullable()->change();
+            $table->json('custom_choices')->nullable()->after('reversed');
+            $table->tinyInteger('old_id')->nullable()->after('order');
+        });
+    }
+};
