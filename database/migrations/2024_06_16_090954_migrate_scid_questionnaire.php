fea40490b0d4c1696b630334bac5c22aeb06531d
carloeusebi <carloeusebi@gmail.com>
2024-06-16 11:24:40 +0200

    Added migration for scid questionnaire

---
+++ database/migrations/2024_06_16_090954_migrate_scid_questionnaire.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Choice;
+use App\Models\Question;
+use App\Models\Questionnaire;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Question::whereNull('text')->delete();
+
+        $scid = Questionnaire::query()
+            ->with('questions.choices')
+            ->withCount('choices')
+            ->firstWhere('title', 'SCID 5-PD Questionario');
+
+        if (!$scid || $scid->choices_count > 0) {
+            return;
+        }
+
+        $scid->questions->first()->choices->each(function (Choice $choice) use ($scid) {
+            $clone = $choice->replicate();
+            $scid->choices()->save($clone);
+        });
+
+        $scid->questions->each(function (Question $question) {
+            $question->choices()->delete();
+        });
+    }
+};
