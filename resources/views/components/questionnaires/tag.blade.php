542e51ce15e1fe520c62a246714ed3c1711ebbef
carloeusebi <carloeusebi@gmail.com>
2024-06-16 12:08:13 +0200

    Removed margin form tag badge

--- resources/views/components/questionnaires/tag.blade.php
+++ resources/views/components/questionnaires/tag.blade.php
@@ -4,7 +4,7 @@
 
 @php /** @var App\Models\Tag $tag */ @endphp
 <div
-    {{ $attributes->class(['badge badge-sm badge-outline my-1 h-fit font-bold']) }}
+    {{ $attributes->class(['badge badge-sm badge-outline h-fit font-bold']) }}
     style="color: {{ $tag->color }}; background-color: {{$tag->color}}20; "
 
 >{{ $tag->name }}</div>
