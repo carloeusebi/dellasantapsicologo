b729a4c46044731de5cb869dea87117d0d6a4b57
carloeusebi <carloeusebi@gmail.com>
2024-06-19 14:46:59 +0200

    Refactored routes file

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,10 @@
 
 use App\Http\Controllers\DownloadSurveyController;
 use App\Http\Controllers\PushController;
-use App\Livewire\Evaluation;
+use App\Livewire\Evaluation\EvaluationPatientForm;
+use App\Livewire\Evaluation\Home;
+use App\Livewire\Evaluation\QuestionnaireScroller;
+use App\Livewire\Evaluation\ThankYou;
 use App\Livewire\Patients\CreatePatient;
 use App\Livewire\Patients\ShowPatient;
 use App\Livewire\Questionnaires\CreateQuestionnaire;
@@ -17,46 +20,35 @@ use Illuminate\Support\Facades\Route;
 Route::view('/offline', 'laravelpwa::offline')->name('offline');
 
 Route::prefix('/test-per-la-valutazione')->name('evaluation.')->group(function () {
-    Route::get('/{survey:token}', Evaluation\Home::class)->name('home');
-    Route::get('/{survey:token}/paziente', Evaluation\EvaluationPatientForm::class)->name('patient');
-    Route::get('/{survey:token}/grazie', Evaluation\ThankYou::class)->name('thank-you');
-    Route::get('/{survey:token}/{questionnaireSurvey}', Evaluation\QuestionnaireScroller::class)->name('questionnaire');
+    Route::get('/{survey:token}', Home::class)->name('home');
+    Route::get('/{survey:token}/paziente', EvaluationPatientForm::class)->name('patient');
+    Route::get('/{survey:token}/grazie', ThankYou::class)->name('thank-you');
+    Route::get('/{survey:token}/{questionnaireSurvey}', QuestionnaireScroller::class)->name('questionnaire');
 });
 
 Route::view('/profilo', 'profile.show-profile')->middleware('auth:web')->name('profile');
 
 Route::middleware(['auth', 'verified'])->group(function () {
 
-    Route::get('/', function () {
-        return to_route('patients.index');
-    })->name('home');
+    Route::get('/', fn() => to_route('patients.index'))->name('home');
 
     Route::get('/download/valutazione/{survey}', DownloadSurveyController::class)->name('download.survey');
 
-    Route::prefix('/pazienti')->name('patients.')->group(function () {
-        Route::view('/', 'patients.index')->name('index');
-        Route::get('/crea', CreatePatient::class)->name('create');
-        Route::get('/{patient}', ShowPatient::class)->name('show');
-    });
+    Route::view('/pazienti', 'patients.index')->name('patients.index');
+    Route::get('/pazienti/crea', CreatePatient::class)->name('patients.create');
+    Route::get('/pazienti/{patient}', ShowPatient::class)->name('patients.show');
 
-    Route::prefix('/valutazioni')->name('surveys.')->group(function () {
-        Route::prefix('/templates')->name('templates.')->group(function () {
-            Route::view('/', 'templates.index')->name('index');
-            Route::get('/crea', CreateTemplate::class)->name('create');
-            Route::get('/{template}', ShowTemplate::class)->name('show');
-        });
+    Route::view('/valutazioni/templates/', 'templates.index')->name('surveys.templates.index');
+    Route::get('/valutazioni/templates/crea', CreateTemplate::class)->name('surveys.templates.create');
+    Route::get('/valutazioni/templates/{template}', ShowTemplate::class)->name('surveys.templates.show');
 
+    Route::view('valutazioni/', 'surveys.index')->name('surveys.index');
+    Route::get('valutazioni/crea', CreateSurvey::class)->name('surveys.create');
+    Route::get('valutazioni/{survey}', ShowSurvey::class)->name('surveys.show');
 
-        Route::view('/', 'surveys.index')->name('index');
-        Route::get('/crea', CreateSurvey::class)->name('create');
-        Route::get('/{survey}', ShowSurvey::class)->name('show');
-    });
-
-    Route::prefix('/questionari')->name('questionnaires.')->group(function () {
-        Route::view('/', 'questionnaires.index')->name('index');
-        Route::get('/crea', CreateQuestionnaire::class)->name('create');
-        Route::get('/{questionnaire}', ShowQuestionnaire::class)->name('show');
-    });
+    Route::view('questionari/', 'questionnaires.index')->name('questionnaires.index');
+    Route::get('questionari/crea', CreateQuestionnaire::class)->name('questionnaires.create');
+    Route::get('questionari/{questionnaire}', ShowQuestionnaire::class)->name('questionnaires.show');
 
     Route::get('/tags', TagsIndex::class)->name('tags.index');
 });
