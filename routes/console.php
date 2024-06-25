d7bdc929b1c7eb9d863b8bdf0e02b6b4a3cdf3d9
carloeusebi <carloeusebi@gmail.com>
2024-06-25 15:31:58 +0200

    Updated console commands frequency

--- routes/console.php
+++ routes/console.php
@@ -4,4 +4,6 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('backup:run --only-db')->daily()->at('00:00');
 Schedule::command('backup:run --only-files')->weekly();
-Schedule::command('backup:clean')->daily()->at('01:00');
+Schedule::command('backup:run --only-files')->lastDayOfMonth();
+Schedule::command('backup:clean')->weeklyOn(6, '23:00');
+
